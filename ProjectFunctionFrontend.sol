1df2022b7b79265386a912b2671bfe2b3b7077e0
AjayGodara2345 <godaraajay2345@gmail.com>
2024-06-29 12:58:23 +0530

    Update ProjectFunctionFrontend.sol

--- ProjectFunctionFrontend.sol
+++ ProjectFunctionFrontend.sol
@@ -1,1 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleContract {
+    string private message;
+    address private owner;
+
+    event MessageChanged(string newMessage);
+
+    constructor(string memory initialMessage) {
+        message = initialMessage;
+        owner = msg.sender;
+    }
+
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+        emit MessageChanged(newMessage);
+    }
+
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+
+    function getOwner() public view returns (address) {
+        return owner;
+    }
+}
 
